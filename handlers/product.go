d88d079237309d15dcb715f3635adda2d9192368
Muhammed Shakir C K <shakirck333@gmail.com>
2022-02-17 20:53:35 +0530

    added restufl api using just basic go packages

--- handlers/product.go
+++ handlers/product.go
@@ -15,10 +15,18 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	log.Println(r.Method)
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJson(rw)
 	if err != nil {
 		http.Error(rw, "unable to marshal json ", http.StatusInternalServerError)
-		return
 	}
 }
